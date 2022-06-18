9bb62ce91f727e82e61bfb18fe3b3d517cd99900
KTM <38443188+k39ajdM2@users.noreply.github.com>
2022-06-18 13:38:45 -0500

    update phyloseq with new R version

--- scripts/2_phyloseq_Flu.R
+++ scripts/2_phyloseq_Flu.R
@@ -14,8 +14,9 @@ library(ggplot2)
 library(dplyr)
 library(tidyr)
 library(tidyverse)
-library(phyloseq)
-library(vegdist)
+library(phyloseq) #installed packages which need compilation
+
+#R version 4.2.0 (2022-04-22)
 
 #######################################################################
 
@@ -60,23 +61,26 @@ class(otu.meta) #Check class type of 'otu.meta'. It should be a dataframe.
 otu.meta2<- cbind(otu.meta) #Make second copy of 'otu.meta' to use to include an "All" column
 otu.meta2$All <- with(otu.meta2, paste0(Day, sep=" ", Treatment)) #Combine "Day" and "Treatment" columns into an "All" column
 head(otu.meta2) #Check first part of otu.meta2
-dim(otu.meta2) #Check dimensions of 'otu.meta2' dataframe
-head(otu.meta2[,1300:1305]) #Check the first part of the end of 'otu.meta2' dataframe
+dim(otu.meta2) #Check dimensions of 'otu.meta2' dataframe; 186 1306
+head(otu.meta2[,1300:1306]) #Check the first part of the end of 'otu.meta2' dataframe
+head(otu.meta2[,1:10])
 rownames(otu.meta2) <- otu.meta2$Row.names #Set "Row.names" as rownames
+otu.meta2 <- otu.meta2[,-1] #remove "Row.names" column
+head(otu.meta2[,1:10])
 otu.meta2 <- otu.meta2[,-1]
 head(otu.meta2[,1:10])
 dim(otu.meta2) #186 1304
 head(otu.meta2[,1300:1304])
 otu.meta2<- otu.meta2[,c(1:4,1304,5:1303)] #Reorder columns to have "All" column after "Treatment" column
 head(otu.meta2[,1:10]) #Check the first part of the beginning of 'otu.meta2' dataframe
-head(otu.meta2[,1300:1303])
+head(otu.meta2[,1300:1304])
 write.csv(otu.meta2, file="SRD129abundsingleton2000.otu.meta.csv")
 
 #Creating phyloseq objects for nasal samples
 
 #Pull out metadata from 'otu.meta2' dataframe
 head(otu.meta2[,1:10])
-dim(otu.meta2) #186 1643
+dim(otu.meta2) #186 1304
 otu.meta3 <- otu.meta2[,1:5] #Take columns 1-5 to make 'otu.meta3'
 head(otu.meta3)
 dim(otu.meta3) #186 5
@@ -123,7 +127,7 @@ otu.notax <- as.matrix(otu.notax) #Turn 'otu.notax' into a matrix class
 class(otu.notax) #matrix
 
 #Create OTU table phyloseq object
-OTU = otu_table(otu.notax, taxa_are_rows = TRUE)
+OTU = otu_table(otu.notax, taxa_are_rows = TRUE, errorIfNULL=TRUE)
 head(OTU)
 dim(OTU) #1299  186
 class(OTU)
@@ -172,50 +176,71 @@ class(adonis.Flu) #data.frame
 dist.Flu <- distance(phyloseqFlu, method="bray") #Distance calculation using Bray-Curtis
 set.seed(1) #Use set.seed function when running simulations to ensure all results are reproducible
 full.Flu <- adonis(dist.Flu~Day*Treatment, data=adonis.Flu, permutations=9999)
+#output message: 'adonis' will be deprecated: use 'adonis2' instead
 full.Flu #Display results
 #Output:
-#Call:
-#Call:
-#  adonis(formula = dist.Flu ~ Day * Treatment, data = adonis.Flu,      permutations = 9999)
-
+#$aov.tab
 #Permutation: free
 #Number of permutations: 9999
 
 #Terms added sequentially (first to last)
 
-#                   Df    SumsOfSqs MeanSqs   F.Model   R2        Pr(>F)
-#  Day              9     8.2853    0.92059   12.8444   0.37628   1e-04 ***
-#  Treatment        1     0.3890    0.38903   5.4279    0.01767   1e-04 ***
-#  Day:Treatment    9     1.4470    0.16077   2.2432    0.06571   1e-04 ***
-#  Residuals        166   11.8977   0.07167             0.54034
-#  Total            185   22.0190                       1.00000
+#               Df SumsOfSqs MeanSqs F.Model      R2 Pr(>F)    
+#Day             9    8.2853 0.92059 12.8444 0.37628  1e-04 ***
+#Treatment       1    0.3890 0.38903  5.4279 0.01767  1e-04 ***
+#Day:Treatment   9    1.4470 0.16077  2.2432 0.06571  1e-04 ***
+#Residuals     166   11.8977 0.07167         0.54034           
+#Total         185   22.0190                 1.00000           
 #---
-#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+#$call
+#adonis(formula = dist.Flu ~ Day * Treatment, data = adonis.Flu, 
+#       permutations = 9999)
+
+#$coefficients
+#NULL
+
+# output continues with coef.sites, f. perms, model.matrix, terms (too long to paste here)
 
 ###Day had largest effect on variation
 
+
+
 #switched Treatment and Day order, obtained same conclusions: Day had the largest effect on variation
 full.Flu_2 <- adonis(dist.Flu~Treatment*Day, data=adonis.Flu, permutations=9999)
+#output message: 'adonis' will be deprecated: use 'adonis2' instead
 full.Flu_2
 #Output:
-#Call:
-#  adonis(formula = dist.Flu ~ Treatment * Day, data = adonis.Flu,      permutations = 9999)
-
+#$aov.tab
 #Permutation: free
 #Number of permutations: 9999
 
 #Terms added sequentially (first to last)
 
-#                   Df    SumsOfSqs MeanSqs   F.Model   R2        Pr(>F)
-#  Treatment        1     0.4068    0.40676   5.6752    0.01847   2e-04 ***
-#  Day              9     8.2676    0.91862   12.8169   0.37548   1e-04 ***
-#  Treatment:Day    9     1.4470    0.16077   2.2432    0.06571   1e-04 ***
-#  Residuals        166   11.8977   0.07167             0.54034
-#  Total            185   22.0190                       1.00000
+#               Df SumsOfSqs MeanSqs F.Model      R2 Pr(>F)    
+#Treatment       1    0.4068 0.40676  5.6752 0.01847  2e-04 ***
+#Day             9    8.2676 0.91862 12.8169 0.37548  1e-04 ***
+#Treatment:Day   9    1.4470 0.16077  2.2432 0.06571  1e-04 ***
+#Residuals     166   11.8977 0.07167         0.54034           
+#Total         185   22.0190                 1.00000           
 #---
-#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+#$call
+#adonis(formula = dist.Flu ~ Treatment * Day, data = adonis.Flu, 
+#       permutations = 9999)
+
+#$coefficients
+#NULL
+
+# output continues with coef.sites, f. perms, model.matrix, terms (too long to paste here)
+
+###Day had largest effect on variation
 
 
+
+### TROUBLESHOOTING ###
 
 #If distance function is giving an error message below:
 #"Error: x should be a data.frame, data.table, tbl, tbl_df, array, or matrix."
